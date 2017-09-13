3e9fc6cfb9912b959f9383ec0482300eefbbf3ad
Banb4n <alban.such@gmail.com>
2017-09-13 18:40:21 +0200

    Add mapsResto.php

---
+++ includes/mapsResto.php
@@ -0,0 +1,8 @@
+<section>
+    <div class="container">
+        <h2>Trouvez votre restaurant BugBurger : </h2>
+    </div>
+    <div class="containerMaps">
+        <iframe src="https://www.google.fr/maps/d/embed?mid=17_9_9l9NcjnUKtyUikTe64EW150" width="100%" height="480"></iframe>
+    </div>
+</section>
