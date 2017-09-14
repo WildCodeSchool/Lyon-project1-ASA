ab3688b94ae71a6157878d2ecbf5c1d6387745e5
Banb4n <alban.such@gmail.com>
2017-09-14 11:45:17 +0200

    Update HEAD of index.php

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" href="./images/logoBUG.png">
     <meta name="viewport" content="width=device-width initial-scale=1.0">
-    <title>BugBurger</title>
+    <title>Acceuil | BugBurger</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
@@ -18,7 +19,7 @@
     ?>
 
     <!-- Jumbotron -->
-    <div class="jumbotron">
+    <div class="jumbotron jumbotronHeader">
         <div class="container-fluid">
             <div class="description">
                 <h1>Bug Burger, 1<sup>er</sup> fast-food d’insecte en France</h1>
