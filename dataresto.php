f47476305141ab276e507d41f8b5c66279a4c5cd
al2b <deboissieu.al@gmail.com>
2017-09-15 09:11:32 +0200

    addo dataresto.php with content for restaurants

---
+++ dataresto.php
@@ -0,0 +1,47 @@
+<?php
+$restaurant = [
+    'lyon' => [
+        'description' => 'C\'est au coeur du 6ème arrondissement de Lyon que se trouve le restaurant, dans un cadre idyllique. ',
+        'horaires' => 'tous les jours',
+        'adresse' => '114 rue Bugeaud',
+        'ville' => 'Lyon',
+    ],
+
+    'paris' => [
+        'description' => 'Oubliez que vous êtes à Paris grâce à notre restaurant !',
+        'horaires' => 'tous les jours',
+        'adresse' => '114 rue Bugeaud',
+        'ville' => 'Paris',
+    ],
+
+    'laloupe' => [
+        'description' => 'Etonnant mais vrai, vous trouverez à La Loupe un Bug Burger très moderne.',
+        'horaires' => 'tous les jours',
+        'adresse' => '114 rue Bugeaud',
+        'ville' => 'La Loupe',
+    ],
+    'strasbourg' => [
+        'description' => 'Etonnant mais vrai, vous trouverez à La Loupe un Bug Burger très moderne.',
+        'horaires' => 'tous les jours',
+        'adresse' => '114 rue Bugeaud',
+        'ville' => 'Strasbourg',
+    ],
+    'orleans' => [
+        'description' => 'Etonnant mais vrai, vous trouverez à La Loupe un Bug Burger très moderne.',
+        'horaires' => 'tous les jours',
+        'adresse' => '114 rue Bugeaud',
+        'ville' => 'Orléans',
+    ],
+    'toulouse' => [
+        'description' => 'Etonnant mais vrai, vous trouverez à La Loupe un Bug Burger très moderne.',
+        'horaires' => 'tous les jours',
+        'adresse' => '114 rue Bugeaud',
+        'ville' => 'Toulouse',
+    ],
+    'Lille' => [
+        'description' => 'Etonnant mais vrai, vous trouverez à La Loupe un Bug Burger très moderne.',
+        'horaires' => 'tous les jours',
+        'adresse' => '114 rue Bugeaud',
+        'ville' => 'Lille',
+    ],
+];
