c20d3c23ca387dcaa88424d8a7c937cb7561d4af
Cverine <severinelab@gmail.com>
2017-09-17 22:20:23 +0200

    Add changes on equipe.php concept.php resto-Lyon.php style.css

--- concept.php
+++ concept.php
@@ -2,6 +2,8 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <!-- TEST AJOUT FONT UBUNTU -->
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
     <link rel="shortcut icon" href="./images/logobug.png">
     <meta name="viewport" content="width=device-width initial-scale=1.0">
     <title>Concept | BugBurger</title>
--- includes/argumentsBarConcept.php
+++ includes/argumentsBarConcept.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: wilder
- * Date: 15/09/17
- * Time: 17:57
- */
+<section id="ArgumentsRow">
+    <div class="containerArg">
+        <article class="itemArg">
+            <i class="fa fa-bug fa-5x logoArg" aria-hidden="true"></i>
+            <h4>Les insectes ? Délicieux !</h4>
+        </article>
+        <article class="itemArg">
+            <i class="fa fa-heart-o fa-5x logoArg" aria-hidden="true"></i>
+            <h4>Les insectes ? Bons pour la santé !</h4>
+        </article>
+        <article class="itemArg">
+            <i class="fa fa-globe fa-5x logoArg" aria-hidden="true"></i>
+            <h4>Les insectes ? Bons pour la planète !</h4>
+        </article>
+    </div>
+</section>
