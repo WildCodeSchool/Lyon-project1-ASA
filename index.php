a7731bb8347c0d7ff263af222f77857e7a581f76
al2b <deboissieu.al@gmail.com>
2017-09-19 14:31:51 +0200

    fix h1 from index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         <div class="container-fluid">
             <div class="container-fluid description">
                 <div class="accrocheHeader">
-                    <h2>Bug Burger, restaurant d’insectes.</h2>
+                    <h1>Bug Burger, restaurant d’insectes.</h1>
                     <p> Une expérience délicieuse pour vous, et bonne pour la planète aussi !</p>
                 </div>
             </div>
